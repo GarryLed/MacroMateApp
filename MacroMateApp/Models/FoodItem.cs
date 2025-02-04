ae2dc8f09885843dfdd5be532dbbe3c57c99573c
GarryLed <s00256356@atu.ie>
2025-02-04 12:39:55 +0000

    Added properties to the foodItem class

--- MacroMateApp/Models/FoodItem.cs
+++ MacroMateApp/Models/FoodItem.cs
@@ -8,5 +8,11 @@ namespace MacroMateApp.Models
 {
     internal class FoodItem
     {
+        // properties 
+        public string Name { get; set; }
+        public double Calories { get; set; }
+        public double Protein { get; set; }
+        public double Carbs { get; set; }
+        public double Fats { get; set; }
     }
 }
