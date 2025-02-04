4fd3dc4ebc0133e1dda4003f1729de553f0792ba
GarryLed <s00256356@atu.ie>
2025-02-04 12:32:02 +0000

    Add project files.

---
+++ MacroMateApp/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using MacroMateApp.Views;
+
+namespace MacroMateApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+
+}
