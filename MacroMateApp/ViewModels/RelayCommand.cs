e70c2146b8dcb14682940b4aa2cc5bced62b7647
GarryLed <s00256356@atu.ie>
2025-02-04 16:48:17 +0000

    created a RelayCommand class to handle binding commands instead of using event handless

---
+++ MacroMateApp/ViewModels/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MacroMateApp.ViewModels
+{
+    // class will handle commands in wpf 
+    // this will replace having to use event handlers in wpf 
+    public class RelayCommand : ICommand // implement ICommand interface to allow buttons to bind to commands in the viewModel 
+    {
+        // properties 
+        private readonly Action _execute; // method runs when the command is executed 
+        private readonly Func<bool> _canExecute;
+
+        // constructor 
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        // methods 
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute();
+        public void Execute(object parameter) => _execute();
+        public event EventHandler CanExecuteChanged;
+    }
+}
