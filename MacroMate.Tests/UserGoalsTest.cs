b6ae283d22911f70cea6e84a58b6263f9d035f5e
GarryLed <S00256356@atu.ie>
2025-04-01 12:06:41 +0100

    Wrote tests for UserGoals

--- MacroMate.Tests/UserGoalsTest.cs
+++ MacroMate.Tests/UserGoalsTest.cs
@@ -8,7 +8,7 @@ namespace MacroMate.Tests
     class UserGoalsTest
     {
         [Test]
-        public void TestUserGoals()
+        public void TestUserGoals() // Test for UserGoals set to specified values
         {
             // Arrange
             var userGoals = new UserGoals
@@ -25,12 +25,46 @@ namespace MacroMate.Tests
             var carbGoal = userGoals.CarbGoal;
             var fatGoal = userGoals.FatGoal;
 
+            // Asser
+            Assert.That(caloriesGoal, Is.EqualTo(2000));
+            Assert.That(proteinGoal, Is.EqualTo(150));
+            Assert.That(carbGoal, Is.EqualTo(250));
+            Assert.That(fatGoal, Is.EqualTo(70));
+               
+        }
+
+        [Test]
+        public void TestUserGoalsPropertyChanged() // Test for property changed event
+        {
+            // Arrange
+            var userGoals = new UserGoals();
+            var propertyChanged = false;
+            userGoals.PropertyChanged += (sender, e) => propertyChanged = true;
+            // Act
+            userGoals.CaloriesGoal = 3000;
             // Assert
-            Assert.Equals(2000, caloriesGoal);
-            Assert.Equals(150, proteinGoal);
-            Assert.Equals(250, carbGoal);
-            Assert.Equals(70, fatGoal);
+            Assert.That(propertyChanged, Is.True);
+        }
+
+        [Test]
+        public void TestUserGoalsSetToDefaultValues() // Test for UserGoals set to default values
+        {
+            // Arrange
+            var userGoals = new UserGoals();
+
+            // Act
+            userGoals.CaloriesGoal = 0;
+            userGoals.ProteinGoal = 0;
+            userGoals.CarbGoal = 0;
+            userGoals.FatGoal = 0;
+
+            // Assert
+            Assert.That(userGoals.CaloriesGoal, Is.EqualTo(0));
+            Assert.That(userGoals.ProteinGoal, Is.EqualTo(0));
+            Assert.That(userGoals.CarbGoal, Is.EqualTo(0));
+            Assert.That(userGoals.FatGoal, Is.EqualTo(0));
             
         }
+       
     }
 }
